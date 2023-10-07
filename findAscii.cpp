4d8b2169c97ddee31fd2f2248276cedad26eead3
Mohan <mohanpandey7557@gmail.com>
2023-10-07 10:49:33 +0530

    Code formatting

--- findAscii.cpp
+++ findAscii.cpp
@@ -1,9 +1,10 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
+int main()
+{
     char ch;
-    cout<<"Enter any character to get the ASCII code for the character : ";
-    cin>>ch;
-    cout<<"The ASCII code for the "<<ch<<" is "<<int(ch);
+    cout << "Enter any character to get the ASCII code for the character : ";
+    cin >> ch;
+    cout << "The ASCII code for the " << ch << " is " << int(ch);
     return 0;
 }
