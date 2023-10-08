fbc21bbceb157d23092335633d9c09e4ec6bb3b2
Mohan <mohanpandey7557@gmail.com>
2023-10-08 22:02:59 +0530

    Another code to get the same result

--- findMissing.cpp
+++ findMissing.cpp
@@ -1,88 +1,115 @@
 #include <iostream>
-#include <algorithm>
 
-int findMissingNumber(int arr[], int n)
+void bubbleSort(int arr[], int n)
 {
-    int minElement = *std::min_element(arr, arr + n);
-    int maxElement = *std::max_element(arr, arr + n);
-
-    bool *present = new bool[maxElement - minElement + 1];
-
-    for (int i = 0; i < maxElement - minElement + 1; i++)
+    for (int i = 0; i < n - 1; i++)
     {
-        present[i] = false;
-    }
-
-    for (int i = 0; i < n; i++)
-    {
-        present[arr[i] - minElement] = true;
-    }
-    for (int i = 0; i < maxElement - minElement + 1; i++)
-    {
-        if (!present[i])
+        for (int j = 0; j < n - i - 1; j++)
         {
-            return i + minElement;
+            if (arr[j] > arr[j + 1])
+            {
+                // Swap arr[j] and arr[j + 1]
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
         }
     }
-
-    delete[] present;
-    return -1; // No missing number found
+}
+int findMissingNumber(int arr[], int size)
+{
+    int firstNumber = arr[0];
+    int j = 1;
+    for (int i = 0; i < size; i++)
+    {
+        if (firstNumber + j != arr[j])
+        {
+            return firstNumber + j;
+        }
+        j++;
+    }
+    return -1;
 }
 
 int main()
 {
-    int n;
-
-    std::cout << "Enter the size of the array: ";
+    int nums[1000], n;
+    std::cout << "Enter the size of the array (Max: 1000) : ";
     std::cin >> n;
-
-    int arr[n];
-
-    std::cout << "Enter the elements of the array:" << std::endl;
     for (int i = 0; i < n; i++)
     {
-        std::cin >> arr[i];
+        std::cout << "Enter Element " << i + 1 << " : ";
+        std::cin >> nums[i];
     }
-
-    int missingNumber = findMissingNumber(arr, n);
-
+    bubbleSort(nums, n);
+    int missingNumber = findMissingNumber(nums, n);
     if (missingNumber != -1)
     {
-        std::cout << "The missing number is: " << missingNumber << std::endl;
+        std::cout << "The missing number is : " << missingNumber;
     }
     else
     {
-        std::cout << "No missing number found." << std::endl;
+        std::cout << "No missing number found";
     }
-
     return 0;
 }
 
 // #include <iostream>
-// using namespace std;
+// #include <algorithm>
+
 // int findMissingNumber(int arr[], int n)
 // {
-//     int i, total;
-//     total = (n + 1) * (n + 2) / 2;
-//     for (i = 0; i < n; i++)
-//         total -= arr[i];
-//     return total;
+//     int minElement = *std::min_element(arr, arr + n);
+//     int maxElement = *std::max_element(arr, arr + n);
+
+//     bool *present = new bool[maxElement - minElement + 1];
+
+//     for (int i = 0; i < maxElement - minElement + 1; i++)
+//     {
+//         present[i] = false;
+//     }
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         present[arr[i] - minElement] = true;
+//     }
+//     for (int i = 0; i < maxElement - minElement + 1; i++)
+//     {
+//         if (!present[i])
+//         {
+//             return i + minElement;
+//         }
+//     }
+
+//     delete[] present;
+//     return -1; // No missing number found
 // }
 
 // int main()
 // {
-//     int nums[1000], n;
-//     cout << "Enter the size of the array (Max: 1000) : ";
-//     cin >> n;
+//     int n;
+
+//     std::cout << "Enter the size of the array: ";
+//     std::cin >> n;
+
+//     int arr[n];
+
+//     std::cout << "Enter the elements of the array:" << std::endl;
 //     for (int i = 0; i < n; i++)
 //     {
-//         cout << "Enter Element " << i + 1 << " : ";
-//         cin >> nums[i];
+//         std::cin >> arr[i];
 //     }
 
-//     int missingNumber = findMissingNumber(nums, n);
+//     int missingNumber = findMissingNumber(arr, n);
 
-//     cout << "The missing number is: " << missingNumber << std::endl;
+//     if (missingNumber != -1)
+//     {
+//         std::cout << "The missing number is: " << missingNumber << std::endl;
+//     }
+//     else
+//     {
+//         std::cout << "No missing number found." << std::endl;
+//     }
 
 //     return 0;
 // }
