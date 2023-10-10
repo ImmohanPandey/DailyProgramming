a3055dabc1404da5409d26dae8391c71e40775da
Mohit Rawat <mohit.rawat2107@gmail.com>
2023-10-10 11:44:34 +0530

    Pandey se behtar

--- FindMissing.py
+++ FindMissing.py
@@ -1,11 +1,11 @@
-import numpy as np
+
 
 n=int(input("Enter size of array : "))
 arr=[]
 for i in range(n-1):
     arr.append(int(input()))
 s=n*(n+1)/2
-print(s-np.sum(arr)) 
+print(int(s-sum(arr))) 
 
 #1235=11
 #12345=15
